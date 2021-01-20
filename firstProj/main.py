81e00bd2681fbe50867e26e6f5620739c5dc6398
escapefromelba <jacobmiller86@hotmail.com>
2021-01-20 11:41:16 -0500

    My first commit

---
+++ firstProj/main.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from chromedriver_py import binary_path
+from selenium.webdriver.common import keys
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Chrome(executable_path=binary_path)
+
+browser.get("https://www.amazon.com/")
+browser.maximize_window()
+browser.find_element_by_id("nav-link-accountList").click()
+browser.find_element_by_name("email").send_keys("jacobmiller86@hotmail.com" + Keys.ENTER)
+browser.find_element_by_name("password").send_keys("R0lltide17!" + Keys.ENTER)
+
+# browser.switch_to_window('https://www.amazon.com/gp/help/customer/display.html?nodeId=508510&ref_=nav_cs_help')
+# assert "Jacob - Google Search" == browser.title
