e185ff6122472daafc82a66ffd2777d0e4abf88e
escapefromelba <jacobmiller86@hotmail.com>
2021-01-20 12:07:05 -0500

    Update main.py

--- firstProj/main.py
+++ firstProj/main.py
@@ -8,8 +8,8 @@ browser = webdriver.Chrome(executable_path=binary_path)
 browser.get("https://www.amazon.com/")
 browser.maximize_window()
 browser.find_element_by_id("nav-link-accountList").click()
-browser.find_element_by_name("email").send_keys("jacobmiller86@hotmail.com" + Keys.ENTER)
-browser.find_element_by_name("password").send_keys("R0lltide17!" + Keys.ENTER)
+browser.find_element_by_name("email").send_keys("username" + Keys.ENTER)
+browser.find_element_by_name("password").send_keys("password" + Keys.ENTER)
 
 # browser.switch_to_window('https://www.amazon.com/gp/help/customer/display.html?nodeId=508510&ref_=nav_cs_help')
 # assert "Jacob - Google Search" == browser.title
